ffb6da896555d78b58f0d17d95b3820118656667
Heikki Pesonen <pesonen.heikki@gmail.com>
2016-10-30 22:00:54 +0200

    add dist

--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+'use strict';
+
 const prefixes = ['moz', 'o', 'ms', 'webkit'];
 
 class ParallaxScroller {
@@ -16,7 +19,7 @@ class ParallaxScroller {
     this._layers = this.layers.map((layer) => this.loadLayer(layer));
 
     this.container.addEventListener('scroll', () => this.applyScroll());
-    window.addEventListener('resize', () => this.resize())
+    window.addEventListener('resize', () => this.resize());
     this.resize();
   }
 
@@ -51,7 +54,7 @@ class ParallaxScroller {
     this.maxScroll = (this.scrollContent.scrollHeight - this.container.innerHeight);
 
     this._layers.forEach((layer) => {
-      layer.movementRatio = this.scrollContent.scrollHeight /  layer.el.offsetHeight;
+      layer.movementRatio = this.scrollContent.scrollHeight / layer.el.offsetHeight;
     });
 
     this.applyScroll();
@@ -62,10 +65,10 @@ class ParallaxScroller {
 
     this._layers.map((layer) => {
       prefixes.forEach((prefix) => {
-        layer.el.style[`-${prefix}-transform`] = `translate3d(0, ${-currentPosition * layer.movementRatio}px, 0)`
+        layer.el.style[`-${prefix}-transform`] = `translate3d(0, ${-currentPosition * layer.movementRatio}px, 0)`;
       });
-      layer.el.style[`transform`] = `translate3d(0, ${-currentPosition * layer.movementRatio}px, 0)`
-    })
+      layer.el.style[`transform`] = `translate3d(0, ${-currentPosition * layer.movementRatio}px, 0)`;
+    });
   }
 
 }
